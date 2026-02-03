8cf2d6f2f4f9df2784023e9d618671b3bde7abd7
Adrian <h7qju7ts@students.codeinstitute.net>
2026-02-03 14:04:53 +0000

    initial commit

---
+++ boat_shaq/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for boat_shaq project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'boat_shaq.settings')
+
+application = get_wsgi_application()
