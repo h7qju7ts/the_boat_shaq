373dd27f491a9ab4a76d52749eaa2478b1e701dc
Adrian <h7qju7ts@students.codeinstitute.net>
2026-02-20 12:55:35 +0000

    added leading slash to static url , add white noise storage

--- boat_shaq/settings.py
+++ boat_shaq/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -172,5 +172,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
