9bbd2b1c2cf60f8900949f40dbc226edc57dd364
Adrian <h7qju7ts@students.codeinstitute.net>
2026-02-20 15:12:48 +0000

    add db to env

--- env.py
+++ env.py
@@ -3,4 +3,4 @@ import os
 os.environ.setdefault('DEVELOPMENT', '1')
 os.environ.setdefault("SECRET_KEY", "(ascvu9zur^)&+grrq*f#1oh2=haibni%m1!-dr%jzk(a@2j&_")
 
-# os.environ.setdefault('DATABASE_URL', 'postgresql://neondb_owner:npg_WyRuhPMeFO06@ep-holy-shadow-agntstuy.c-2.eu-central-1.aws.neon.tech/blush_crane_stray_165775')
+os.environ.setdefault('DATABASE_URL', 'postgresql://neondb_owner:npg_WyRuhPMeFO06@ep-holy-shadow-agntstuy.c-2.eu-central-1.aws.neon.tech/blush_crane_stray_165775')
