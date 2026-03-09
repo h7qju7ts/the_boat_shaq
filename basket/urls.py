ee5d49cf20b20f0113b4f996aa9869d074ef59ef
Adrian <h7qju7ts@students.codeinstitute.net>
2026-03-09 14:10:57 +0000

    add views and paths to basket, reorganise catalog cards

--- basket/urls.py
+++ basket/urls.py
@@ -4,6 +4,9 @@ from . import views
 app_name = "basket"
 
 urlpatterns = [
-    path("add/<int:boat_id>/", views.add_to_basket, name="add_to_basket"),
     path("", views.view_basket, name="basket"),
+    path("add/<int:boat_id>/", views.add_to_basket, name="add_to_basket"),
+    path("decrease/<int:boat_id>/", views.decrease_basket, name="decrease_basket"),
+    path("remove/<int:boat_id>/", views.remove_from_basket, name="remove_from_basket"),
+    path("clear/", views.clear_basket, name="clear_basket"),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
+from django.views.decorators.http import require_POST
 from boats.models import Boat
 
-# Create your views here.
+
+@require_POST
 def add_to_basket(request, boat_id):
     boat = get_object_or_404(Boat, pk=boat_id)
 
@@ -17,6 +19,33 @@ def add_to_basket(request, boat_id):
 
     redirect_url = request.POST.get("redirect_url")
     return redirect(redirect_url or "boats:catalog")
+
+
+@require_POST
+def decrease_basket(request, boat_id):
+    basket = request.session.get("basket", {})
+    boat_id = str(boat_id)
+
+    if boat_id in basket:
+        basket[boat_id] -= 1
+        if basket[boat_id] <= 0:
+            del basket[boat_id]
+
+    request.session["basket"] = basket
+    return redirect("basket:basket")
+
+
+@require_POST
+def remove_from_basket(request, boat_id):
+    basket = request.session.get("basket", {})
+    boat_id = str(boat_id)
+
+    if boat_id in basket:
+        del basket[boat_id]
+
+    request.session["basket"] = basket
+    return redirect("basket:basket")
+
 
 def view_basket(request):
     basket = request.session.get("basket", {})
@@ -37,6 +66,12 @@ def view_basket(request):
     context = {
         "basket_items": basket_items,
         "total": total,
-    }    
+    }
 
     return render(request, "basket/basket.html", context)
+
+
+@require_POST
+def clear_basket(request):
+    request.session["basket"] = {}
+    return redirect("basket:basket")
